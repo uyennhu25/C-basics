149bb27aaa3e3900a43c9bb96bffa4615aad8956
uyennhu25 <nguyenuyennhususu@gmail.com>
2024-07-06 22:48:07 +0700

    thi cuoi mon1

---
+++ Lab/test2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point_t
+{
+    int x;
+    int y;
+};
+
+
+int main () {
+    int *ptr;
+    ptr = (int*)malloc(sizeof(int));
+    
+    FILE *pfile = NULL;
+    
+}
+
