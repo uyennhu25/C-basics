3a97c32b3c9647859877ad3a97f6a537c6aef28a
uyennhu25 <nguyenuyennhususu@gmail.com>
2024-07-07 22:56:54 +0700

    tested leetcode

--- Lab/test2.c
+++ Lab/test2.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct point_t
-{
-    int x;
-    int y;
-};
-
+#include <stdbool.h>
+int* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize);
 
 int main () {
-    int *ptr;
-    ptr = (int*)malloc(sizeof(int));
+    int arr[] = {2,3,5,1,3};
+    int* candies = arr;
+    int* returnSize;
+    kidsWithCandies(candies, 5, 3, returnSize);
+    for (int i = 0; i < 5; i++) {
+        printf("%d", returnSize[i]);
+    }
     
-    FILE *pfile = NULL;
-    
+}
+int* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+    int max = 0;
+    for (int i = 0; i < candiesSize; i++) {
+        if (candies[i]>max) {
+            max = candies[i];
+        }
+    }
+    returnSize = (int*)malloc(candiesSize*sizeof(int));
+    for (int i = 0; i < candiesSize; i++) {
+        if ((candies[i] + extraCandies) >= max) {
+            returnSize[i] = 1;
+        } else {
+            returnSize[i] = 0;
+        }
+    }
+    return returnSize;
 }
 
