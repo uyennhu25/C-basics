e361487b146a811b0bb7f62fcebf6747d6770de1
uyennhu25 <nguyenuyennhususu@gmail.com>
2024-07-17 23:11:05 +0700

    python3 binary

--- Lab/binarytree.c
+++ Lab/binarytree.c
@@ -26,3 +26,21 @@ struct Node* insert(struct Node* node, int data) {
     }
     return node;
 }
+
+/*
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        to_delete_set = set(to_delete)
+        res = []
+
+        def helper(root, is_root):
+            if not root:
+                return None
+            root_deleted = root.val in to_delete_set
+            if is_root and not root_deleted:
+                res.append(root)
+            root.left = helper(root.left, root_deleted)
+            root.right = helper(root.right, root_deleted)
+            return None if root_deleted else root
+        helper(root, True)
+        return res*/
